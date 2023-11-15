04725ffa67bca6f86691617a97ed804fcebaad6b
MishachS <sashamih12@yandex.ru>
2023-11-16 00:06:25 +0300

    Исправил замечания pull-request 2

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -1,11 +1,11 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message){
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
         super(message);
     }
 
-    public NotFoundException(){
+    public NotFoundException() {
         super();
     }
 }
